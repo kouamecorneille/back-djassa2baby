baf9f6828e14f3710b5153646947dbe6e16b6fdb
kouamecorneille <smcorneille@gmail.com>
2024-06-07 17:20:35 +0000

    mes modifs

---
+++ backoffice/src/app/pages/vendors/store-infos/store-infos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-infos',
+  standalone: true,
+  imports: [],
+  templateUrl: './store-infos.component.html',
+  styleUrl: './store-infos.component.css'
+})
+export class StoreInfosComponent {
+
+}
--- backoffice/src/app/pages/vendors/store-settings/store-settings.component.ts
+++ backoffice/src/app/pages/vendors/store-settings/store-settings.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb.component';
+import { StoreInfosComponent } from '../store-infos/store-infos.component';
+import { UserProfilComponent } from '../../auth/user-profil/user-profil.component';
 
 @Component({
   selector: 'app-store-settings',
   standalone: true,
-  imports: [BreadcrumbComponent],
+  imports: [BreadcrumbComponent,StoreInfosComponent,UserProfilComponent],
   templateUrl: './store-settings.component.html',
   styleUrl: './store-settings.component.css'
 })
