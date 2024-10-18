8633ae16b527086843d815c4410c988d828b52e3
kouamecorneille <smcorneille@gmail.com>
2024-10-18 15:04:09 +0000

    me smodis

--- backoffice/src/environments/environment.ts
+++ backoffice/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  apiUrl: 'http://127.0.0.1:8002/api',
   baseUrl: 'http://127.0.0.1:8002',
 };
 
